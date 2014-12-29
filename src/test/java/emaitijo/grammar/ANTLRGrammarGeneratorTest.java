135b313b4a1cc5e22fc480de2b90176b2b05f880
Bérenger Dulac <berenger.dulac@laposte.net>
2014-12-29 20:54:05 +0100

    Tests improvement

--- src/test/java/emaitijo/grammar/ANTLRGrammarGeneratorTest.java
+++ src/test/java/emaitijo/grammar/ANTLRGrammarGeneratorTest.java
@@ -9,12 +9,32 @@ import junit.framework.TestCase;
 public class ANTLRGrammarGeneratorTest extends TestCase {
 	
 	private static Logger logger = Logger.getAnonymousLogger();
-
-	public void testWriteGrammarFromFile() throws IOException {
+	
+	public void testTransformBnf2Grammar_coma() throws IOException {
+		ANTLRGrammarGenerator gen = new ANTLRGrammarGenerator();
+		String res = gen.transformBnf2Grammar("coma.bnf");
+		assertNotNull(res);
+		logger.info(res);
+	}
+	
+	public void testTransformBnf2Grammar_option() throws IOException {
+		ANTLRGrammarGenerator gen = new ANTLRGrammarGenerator();
+		String res = gen.transformBnf2Grammar("option.bnf");
+		assertNotNull(res);
+		logger.info(res);
+	}
+	
+	public void testTransformBnf2Grammar_alternation() throws IOException {
+		ANTLRGrammarGenerator gen = new ANTLRGrammarGenerator();
+		String res = gen.transformBnf2Grammar("alternation.bnf");
+		assertNotNull(res);
+		logger.info(res);
+	}
+	
+	public void testTransformBnf2Grammar_jpql() throws IOException {
 		ANTLRGrammarGenerator gen = new ANTLRGrammarGenerator();
 		String res = gen.transformBnf2Grammar("jpql.bnf");
 		assertNotNull(res);
 		logger.info(res);
 	}
-	
 }
